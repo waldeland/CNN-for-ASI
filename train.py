2c417acbcd5ab94ab6023c7cf5751f933e43d7dd
Uzair Aftab <muaf@nmbu.no>
2020-06-12 14:27:51 +0200

    Added code  that asks enduser if they want to use gpu or not

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 # Compatability Imports
-# test
+
 from __future__ import print_function
 from os.path import join
 
@@ -23,7 +23,15 @@ from utils import *
 dataset_name = 'F3'
 im_size = 65
 batch_size = 32 #If you have a GPU with little memory, try reducing this to 16 (may degrade results)
-use_gpu = False #Switch to toggle the use of GPU or not
+
+mode = input("Do you want to run the model on your gpu? yes or no?")
+mode = mode.lower()
+if mode == "yes":
+    use_gpu = True
+elif mode == "no":
+    use_gpu = False
+
+#use_gpu = False #Switch to toggle the use of GPU or not
 log_tensorboard = True #Log progress on tensor board
 if log_tensorboard: logger = tb_logger.TBLogger('log', 'Train')
 
