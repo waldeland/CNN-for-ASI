9ecd6afd9ae9b615a0288e83e176daf97326ebd3
Uzair Aftab <muaf@nmbu.no>
2020-06-13 16:49:49 +0200

    Clean Up

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ dataset_name = 'F3'
 im_size = 65
 batch_size = 32 #If you have a GPU with little memory, try reducing this to 16 (may degrade results)
 
-use_gpu = True #Switch to toggle the use of GPU or not
+use_gpu = False #Switch to toggle the use of GPU or not
 log_tensorboard = True #Log progress on tensor board
 if log_tensorboard: logger = tb_logger.TBLogger('log', 'Train')
 
